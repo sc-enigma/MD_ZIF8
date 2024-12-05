97430aefec31d2b71f86c708be72afde7df1cca0
sc-enigma <153753211+sc-enigma@users.noreply.github.com>
2024-12-05 11:47:18 +0700

    Add files via upload

---
+++ TOP/Guests/Gromacs/print_atomtypes.py
@@ -0,0 +1,391 @@
+etb_strings = [
+'     1   etbf_800      1    ETB   CT0      1    -0.2328    12.0110 ',
+'     2   etbf_801      1    ETB   CT1      1    -0.0796    12.0110 ',
+'     3   etbf_802      1    ETB   CA2      1    -0.0796    12.0110 ',
+'     4   etbf_803      1    ETB   CA3      1    -0.1447    12.0110 ',
+'     5   etbf_804      1    ETB   CA4      1    -0.1461    12.0110 ',
+'     6   etbf_805      1    ETB   CA5      1    -0.1514    12.0110 ',
+'     7   etbf_806      1    ETB   CA6      1    -0.1459    12.0110 ',
+'     8   etbf_807      1    ETB   CA7      1    -0.1441    12.0110 ',
+'     9   etbf_808      1    ETB   HC8      1     0.0845     1.0080 ', 
+'    10   etbf_809      1    ETB   HC9      1     0.0845     1.0080 ', 
+'    11   etbf_810      1    ETB   HCA      1     0.0845     1.0080 ', 
+'    12   etbf_811      1    ETB   HCB      1     0.0957     1.0080 ', 
+'    13   etbf_812      1    ETB   HCC      1     0.0957     1.0080 ', 
+'    14   etbf_813      1    ETB   HAD      1     0.1475     1.0080 ', 
+'    15   etbf_814      1    ETB   HAE      1     0.1473     1.0080 ', 
+'    16   etbf_815      1    ETB   HAF      1     0.1467     1.0080 ', 
+'    17   etbf_816      1    ETB   HAG      1     0.1467     1.0080 ', 
+'    18   etbf_817      1    ETB   HAH      1     0.1477     1.0080 ']
+
+hex_strings = [
+'     1   hexf_800      1    HEX   CT0      1    -0.2387    12.0110 ',
+'     2   hexf_801      1    HEX   CT1      1    -0.1793    12.0110 ',
+'     3   hexf_802      1    HEX   CT2      1    -0.1785    12.0110 ',
+'     4   hexf_803      1    HEX   CT3      1    -0.1785    12.0110 ',
+'     5   hexf_804      1    HEX   CT4      1    -0.1793    12.0110 ',
+'     6   hexf_805      1    HEX   CT5      1    -0.2387    12.0110 ',
+'     7   hexf_806      1    HEX   HC6      1     0.0807     1.0080 ', 
+'     8   hexf_807      1    HEX   HC7      1     0.0807     1.0080 ', 
+'     9   hexf_808      1    HEX   HC8      1     0.0807     1.0080 ', 
+'    10   hexf_809      1    HEX   HC9      1     0.0881     1.0080 ', 
+'    11   hexf_810      1    HEX   HCA      1     0.0881     1.0080 ', 
+'    12   hexf_811      1    HEX   HCB      1     0.0891     1.0080 ', 
+'    13   hexf_812      1    HEX   HCC      1     0.0891     1.0080 ', 
+'    14   hexf_813      1    HEX   HCD      1     0.0891     1.0080 ', 
+'    15   hexf_814      1    HEX   HCE      1     0.0891     1.0080 ', 
+'    16   hexf_815      1    HEX   HCF      1     0.0881     1.0080 ', 
+'    17   hexf_816      1    HEX   HCG      1     0.0881     1.0080 ', 
+'    18   hexf_817      1    HEX   HCH      1     0.0807     1.0080 ', 
+'    19   hexf_818      1    HEX   HCI      1     0.0807     1.0080 ', 
+'    20   hexf_819      1    HEX   HCJ      1     0.0807     1.0080 ']
+
+mes_strings = [
+'     1   mesf_800      1    MES   CT0      1    -0.1972    12.0110 ',
+'     2   mesf_801      1    MES   CA1      1    -0.0753    12.0110 ',
+'     3   mesf_802      1    MES   CA2      1    -0.1504    12.0110 ',
+'     4   mesf_803      1    MES   CA3      1    -0.0757    12.0110 ',
+'     5   mesf_804      1    MES   CT4      1    -0.1970    12.0110 ',
+'     6   mesf_805      1    MES   CA5      1    -0.1528    12.0110 ',
+'     7   mesf_806      1    MES   CA6      1    -0.0743    12.0110 ',
+'     8   mesf_807      1    MES   CT7      1    -0.1972    12.0110 ',
+'     9   mesf_808      1    MES   CA8      1    -0.1551    12.0110 ',
+'    10   mesf_809      1    MES   HC9      1     0.0928     1.0080 ', 
+'    11   mesf_810      1    MES   HCA      1     0.0928     1.0080 ', 
+'    12   mesf_811      1    MES   HCB      1     0.0928     1.0080 ', 
+'    13   mesf_812      1    MES   HAC      1     0.1466     1.0080 ', 
+'    14   mesf_813      1    MES   HCD      1     0.0928     1.0080 ', 
+'    15   mesf_814      1    MES   HCE      1     0.0928     1.0080 ', 
+'    16   mesf_815      1    MES   HCF      1     0.0928     1.0080 ', 
+'    17   mesf_816      1    MES   HAG      1     0.1466     1.0080 ', 
+'    18   mesf_817      1    MES   HCH      1     0.0928     1.0080 ', 
+'    19   mesf_818      1    MES   HCI      1     0.0928     1.0080 ', 
+'    20   mesf_819      1    MES   HCJ      1     0.0928     1.0080 ', 
+'    21   mesf_820      1    MES   HAK      1     0.1466     1.0080 ']
+
+mxl_strings = [
+'     1   mxlf_800      1    MXL   CT0      1    -0.1977    12.0110 ',
+'     2   mxlf_801      1    MXL   CA1      1    -0.0775    12.0110 ',
+'     3   mxlf_802      1    MXL   CA2      1    -0.1518    12.0110 ',
+'     4   mxlf_803      1    MXL   CA3      1    -0.1423    12.0110 ',
+'     5   mxlf_804      1    MXL   CA4      1    -0.1517    12.0110 ',
+'     6   mxlf_805      1    MXL   CA5      1    -0.0770    12.0110 ',
+'     7   mxlf_806      1    MXL   CT6      1    -0.1979    12.0110 ',
+'     8   mxlf_807      1    MXL   CA7      1    -0.1499    12.0110 ',
+'     9   mxlf_808      1    MXL   HC8      1     0.0930     1.0080 ', 
+'    10   mxlf_809      1    MXL   HC9      1     0.0930     1.0080 ', 
+'    11   mxlf_810      1    MXL   HCA      1     0.0930     1.0080 ', 
+'    12   mxlf_811      1    MXL   HAB      1     0.1466     1.0080 ', 
+'    13   mxlf_812      1    MXL   HAC      1     0.1472     1.0080 ', 
+'    14   mxlf_813      1    MXL   HAD      1     0.1467     1.0080 ', 
+'    15   mxlf_814      1    MXL   HCE      1     0.0930     1.0080 ', 
+'    16   mxlf_815      1    MXL   HCF      1     0.0930     1.0080 ', 
+'    17   mxlf_816      1    MXL   HCG      1     0.0930     1.0080 ', 
+'    18   mxlf_817      1    MXL   HAH      1     0.1473     1.0080 ']
+
+oxl_strings = [
+'     1   oxlf_800      1    OXL   CT0      1    -0.1958    12.0110 ',
+'     2   oxlf_801      1    OXL   CA1      1    -0.0798    12.0110 ',
+'     3   oxlf_802      1    OXL   CA2      1    -0.1452    12.0110 ',
+'     4   oxlf_803      1    OXL   CA3      1    -0.1502    12.0110 ',
+'     5   oxlf_804      1    OXL   CA4      1    -0.1502    12.0110 ',
+'     6   oxlf_805      1    OXL   CA5      1    -0.1445    12.0110 ',
+'     7   oxlf_806      1    OXL   CA6      1    -0.0794    12.0110 ',
+'     8   oxlf_807      1    OXL   CT7      1    -0.1954    12.0110 ',
+'     9   oxlf_808      1    OXL   HC8      1     0.0923     1.0080 ', 
+'    10   oxlf_809      1    OXL   HC9      1     0.0923     1.0080 ', 
+'    11   oxlf_810      1    OXL   HCA      1     0.0923     1.0080 ', 
+'    12   oxlf_811      1    OXL   HAB      1     0.1468     1.0080 ', 
+'    13   oxlf_812      1    OXL   HAC      1     0.1466     1.0080 ', 
+'    14   oxlf_813      1    OXL   HAD      1     0.1468     1.0080 ', 
+'    15   oxlf_814      1    OXL   HAE      1     0.1462     1.0080 ', 
+'    16   oxlf_815      1    OXL   HCF      1     0.0924     1.0080 ', 
+'    17   oxlf_816      1    OXL   HCG      1     0.0924     1.0080 ', 
+'    18   oxlf_817      1    OXL   HCH      1     0.0924     1.0080 ']
+
+pxl_strings = [
+'     1   pxlf_800      1    PXL   CT0      1    -0.1974    12.0110 ',
+'     2   pxlf_801      1    PXL   CA1      1    -0.0861    12.0110 ',
+'     3   pxlf_802      1    PXL   CA2      1    -0.1439    12.0110 ',
+'     4   pxlf_803      1    PXL   CA3      1    -0.1438    12.0110 ',
+'     5   pxlf_804      1    PXL   CA4      1    -0.0859    12.0110 ',
+'     6   pxlf_805      1    PXL   CT5      1    -0.1970    12.0110 ',
+'     7   pxlf_806      1    PXL   CA6      1    -0.1451    12.0110 ',
+'     8   pxlf_807      1    PXL   CA7      1    -0.1452    12.0110 ',
+'     9   pxlf_808      1    PXL   HC8      1     0.0926     1.0080 ', 
+'    10   pxlf_809      1    PXL   HC9      1     0.0926     1.0080 ', 
+'    11   pxlf_810      1    PXL   HCA      1     0.0926     1.0080 ', 
+'    12   pxlf_811      1    PXL   HAB      1     0.1474     1.0080 ', 
+'    13   pxlf_812      1    PXL   HAC      1     0.1472     1.0080 ', 
+'    14   pxlf_813      1    PXL   HCD      1     0.0926     1.0080 ', 
+'    15   pxlf_814      1    PXL   HCE      1     0.0926     1.0080 ', 
+'    16   pxlf_815      1    PXL   HCF      1     0.0926     1.0080 ', 
+'    17   pxlf_816      1    PXL   HAG      1     0.1468     1.0080 ', 
+'    18   pxlf_817      1    PXL   HAH      1     0.1474     1.0080 ']
+
+pdb_strings = [
+'     1   pdbf_800      1    PDB   CT0      1    -0.2339    12.0110 ',
+'     2   pdbf_801      1    PDB   CT1      1    -0.1352    12.0110 ',
+'     3   pdbf_802      1    PDB   CA2      1    -0.0851    12.0110 ',
+'     4   pdbf_803      1    PDB   CA3      1     -0.133    12.0110 ',
+'     5   pdbf_804      1    PDB   CA4      1    -0.1331    12.0110 ',
+'     6   pdbf_805      1    PDB   CA5      1     -0.084    12.0110 ',
+'     7   pdbf_806      1    PDB   CT6      1    -0.1354    12.0110 ',
+'     8   pdbf_807      1    PDB   CT7      1    -0.2322    12.0110 ',
+'     9   pdbf_808      1    PDB   CA8      1    -0.1326    12.0110 ',
+'    10   pdbf_809      1    PDB   CA9      1    -0.1336    12.0110 ',
+'    11   pdbf_810      1    PDB   HCA      1     0.0841     1.0080 ', 
+'    12   pdbf_811      1    PDB   HCB      1     0.0841     1.0080 ', 
+'    13   pdbf_812      1    PDB   HCC      1     0.0841     1.0080 ', 
+'    14   pdbf_813      1    PDB   HCD      1      0.096     1.0080 ', 
+'    15   pdbf_814      1    PDB   HCE      1      0.096     1.0080 ', 
+'    16   pdbf_815      1    PDB   HAF      1     0.1375     1.0080 ', 
+'    17   pdbf_816      1    PDB   HAG      1     0.1374     1.0080 ', 
+'    18   pdbf_817      1    PDB   HCH      1     0.0951     1.0080 ', 
+'    19   pdbf_818      1    PDB   HCI      1     0.0951     1.0080 ', 
+'    20   pdbf_819      1    PDB   HCJ      1     0.0842     1.0080 ', 
+'    21   pdbf_820      1    PDB   HCK      1     0.0842     1.0080 ', 
+'    22   pdbf_821      1    PDB   HCM      1     0.0842     1.0080 ', 
+'    23   pdbf_822      1    PDB   HAN      1     0.1374     1.0080 ', 
+'    24   pdbf_823      1    PDB   HAO      1     0.1387     1.0080 ']
+
+tol_strings = [
+'     1   tolf_800      1    TOL   CT0      1    -0.1984    12.0110 ',
+'     2   tolf_801      1    TOL   CA1      1    -0.0803    12.0110 ',
+'     3   tolf_802      1    TOL   CA2      1    -0.1468    12.0110 ',
+'     4   tolf_803      1    TOL   CA3      1    -0.1445    12.0110 ',
+'     5   tolf_804      1    TOL   CA4      1    -0.1530    12.0110 ',
+'     6   tolf_805      1    TOL   CA5      1    -0.1437    12.0110 ',
+'     7   tolf_806      1    TOL   CA6      1    -0.1487    12.0110 ',
+'     8   tolf_807      1    TOL   HC7      1     0.0933     1.0080 ', 
+'     9   tolf_808      1    TOL   HC8      1     0.0933     1.0080 ', 
+'    10   tolf_809      1    TOL   HC9      1     0.0933     1.0080 ', 
+'    11   tolf_810      1    TOL   HAA      1     0.1474     1.0080 ', 
+'    12   tolf_811      1    TOL   HAB      1     0.1473     1.0080 ', 
+'    13   tolf_812      1    TOL   HAC      1     0.1468     1.0080 ', 
+'    14   tolf_813      1    TOL   HAD      1     0.1467     1.0080 ', 
+'    15   tolf_814      1    TOL   HAE      1     0.1473     1.0080 ']
+
+etb_charges = [
+'0.1483',
+'0.0263',
+'0.0829',
+'-0.1531',
+'-0.0800',
+'-0.0860',
+'-0.0800',
+'-0.1531',
+'-0.0296',
+'-0.0492',
+'-0.0492',
+'-0.0129',
+'-0.0129',
+'0.1038',
+'0.0802',
+'0.0805',
+'0.0802',
+'0.1038']
+
+hex_charges = [
+'-0.1924',
+'0.2348',
+'-0.0171',
+'-0.0171',
+'0.2348',
+'-0.1924',
+'0.0355',
+'0.0355',
+'0.0355',
+'-0.0546',
+'-0.0546',
+'-0.0113',
+'-0.0113',
+'-0.0113',
+'-0.0113',
+'-0.0546',
+'-0.0546',
+'0.0355',
+'0.0355',
+'0.0355']
+
+mes_charges = [
+'-0.1985',
+'0.2824',
+'-0.3895',
+'0.28225',
+'-0.2023',
+'-0.3897',
+'0.28225',
+'-0.2023',
+'-0.3895',
+'0.0541',
+'0.0541',
+'0.0541',
+'0.1438',
+'0.0704',
+'0.0704',
+'0.0704',
+'0.0704',
+'0.0704',
+'0.0704',
+'0.0526',
+'0.1438']
+
+mxl_charges = [
+'-0.2400',
+'0.2547',
+'-0.2590',
+'-0.0103',
+'-0.2590',
+'0.2547',
+'-0.2400',
+'-0.3372',
+'0.0650',
+'0.0650',
+'0.0650',
+'0.1123',
+'0.0789',
+'0.1123',
+'0.0650',
+'0.0650',
+'0.0650',
+'0.1426']
+
+oxl_charges = [
+'-0.2289',
+'0.1093',
+'-0.1849',
+'-0.0925',
+'-0.0925',
+'-0.1849',
+'0.1093',
+'-0.2289',
+'0.0676',
+'0.0676',
+'0.0676',
+'0.1073',
+'0.0869',
+'0.0869',
+'0.1073',
+'0.0676',
+'0.0676',
+'0.0676']
+
+pxl_charges = [
+'-0.2270',
+'0.1841',
+'-0.1805',
+'-0.1805',
+'0.1841',
+'-0.2270',
+'-0.1805',
+'-0.1805',
+'0.0627',
+'0.0627',
+'0.0627',
+'0.1079',
+'0.1079',
+'0.0627',
+'0.0627',
+'0.0627',
+'0.1079',
+'0.1079']
+
+pdb_charges = [
+'0.1219',
+'0.0316',
+'0.0822',
+'-0.1578',
+'-0.1578',
+'0.0822',
+'0.0316',
+'0.1219',
+'-0.1578',
+'-0.1578',
+'-0.0365',
+'-0.0365',
+'-0.0365',
+'-0.0120',
+'-0.0120',
+'0.1067',
+'0.1067',
+'-0.0120',
+'-0.0120',
+'-0.0365',
+'-0.0365',
+'-0.0365',
+'0.1067',
+'0.1067']
+
+tol_charges = [
+'-0.2720',
+'0.2337',
+'-0.2100',
+'-0.0476',
+'-0.1232',
+'-0.0476',
+'-0.2100',
+'0.0737',
+'0.0737',
+'0.0737',
+'0.1067',
+'0.0781',
+'0.0862',
+'0.0781',
+'0.1067']
+
+for idx in range(len(etb_strings)):
+    split = etb_strings[idx].split()
+    charge = etb_charges[idx]
+    etb_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(etb_strings[idx])
+print('')
+    
+for idx in range(len(hex_strings)):
+    split = hex_strings[idx].split()
+    charge = hex_charges[idx]
+    hex_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(hex_strings[idx])
+print('')
+
+for idx in range(len(mes_strings)):
+    split = mes_strings[idx].split()
+    charge = mes_charges[idx]
+    mes_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(mes_strings[idx])
+print('')
+
+for idx in range(len(mxl_strings)):
+    split = mxl_strings[idx].split()
+    charge = mxl_charges[idx]
+    mxl_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(mxl_strings[idx])
+print('')
+
+for idx in range(len(oxl_strings)):
+    split = oxl_strings[idx].split()
+    charge = oxl_charges[idx]
+    oxl_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(oxl_strings[idx])
+print('')
+
+for idx in range(len(pxl_strings)):
+    split = pxl_strings[idx].split()
+    charge = pxl_charges[idx]
+    pxl_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(pxl_strings[idx])
+print('')
+
+for idx in range(len(pdb_strings)):
+    split = pdb_strings[idx].split()
+    charge = pdb_charges[idx]
+    pdb_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(pdb_strings[idx])
+print('')
+
+for idx in range(len(tol_strings)):
+    split = tol_strings[idx].split()
+    charge = tol_charges[idx]
+    tol_strings[idx] = f'{split[0]:>6}{split[1]:>11}{split[2]:>7}{split[3]:>7}{split[4]:>6}{split[5]:>7}{charge:>11}{split[7]:>11}'
+    print(tol_strings[idx])
+print('')
